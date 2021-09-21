95ac53358c0b0123b29f9b615720afae25abb70b
skylerdj <jeffreyjoumjian@hotmail.com>
2021-09-21 23:25:39 +0300

    final v1.0.0

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ const GameManager = require('./game.js');
 
 const server = require('http').createServer(app);
 
-let corsOptions = { cors: { origin: ["http://127.0.0.1:3001", "http://localhost:3001"] } };
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-if (process.env.NODE_ENV === "production") {
-
-	app.use(express.static(path.join(__dirname, "./client/build")));
-}
-
+// let corsOptions = { cors: { origin: ["http://127.0.0.1:3001", "http://localhost:3001"] } };
+// app.use(express.static(path.join(__dirname, "./client/build")));
 // const io = require('socket.io')(server, corsOptions); // dev
+
+if (process.env.NODE_ENV === "production")
+	app.use(express.static(path.join(__dirname, "./client/build")));
+
 const io = require('socket.io')(server); // prod
 
 const connections = [null, null];
